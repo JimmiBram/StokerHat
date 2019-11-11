8abf248b53a984dd687e3a7da1ec3b501ab08910
Jimmi Bram <post@jimmibram.dk>
2019-11-11 13:38:18 +0100

    Adding service structur

---
+++ HatService/HatServiceInstance.cs
@@ -0,0 +1,12 @@
+public class HatServiceInstance{
+    //Propperties
+    public bool IsRunning { get; set; }
+    
+    //Internal Classes
+    DataUnit CollectSample(){
+
+        return new DataUnit();
+    }
+
+    
+}
---
+++ HatService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HatService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HatService starting..");
+            HatService _service = new HatService();
+            Console.WriteLine("Service ready");
+            while(_service.isRunning){
+                //Looping forever, or at least for a good while
+            }
+            
+        }
+    }
+}
